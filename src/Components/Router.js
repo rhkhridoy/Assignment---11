d8acfdb547fc4a2963c77b42fe13745508437045
Md. Nasir Ullah Hridoy <mdnasirullahhridoy@gmail.com>
2022-11-10 23:28:44 +0600

    router done

---
+++ src/Components/Router.js
@@ -0,0 +1,70 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import AddServices from "./AddServices";
+import Blog from "./Blog";
+import ErrorPage from "./ErrorPage";
+import Home from "./Home/Home";
+import Login from "./Login";
+import MyReview from "./MyReview";
+import Register from "./Register";
+import ReviewEdit from "./ReviewEdit";
+import ServiceDetails from "./ServiceDetails";
+import TotalServices from "./TotalServices/TotalServices";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/totalServices',
+           
+                element: <TotalServices></TotalServices>
+            },
+
+            {
+                path: '/service/:id',
+                loader: ({ params }) => fetch(`https://y-two-woad.vercel.app/totalServices/${params.id}`),
+                element: <ServiceDetails></ServiceDetails>
+            },
+
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+
+            {
+                path: '/myReview',
+                element: <PrivateRoute><MyReview></MyReview></PrivateRoute>
+            },
+            {
+                path: '/editReview/:id',
+                element: <ReviewEdit></ReviewEdit>,
+                loader: ({ params }) => fetch(`https://y-two-woad.vercel.app/review/${params.id}`)
+            },
+
+            {
+                path: '/addService',
+                element: <PrivateRoute><AddServices></AddServices></PrivateRoute>
+            }
+
+        ]
+
+    }
+
+])
